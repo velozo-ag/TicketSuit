ba3f4b205f99357dfd4366032c5caf1e75dd8d1a
n9bes <mateoismael310@gmail.com>
2024-09-28 21:51:11 -0300

    PanelGerente & Gerente Controller, Archivos base de la vista gerente

---
+++ src/views/gerente/ChartsIngresosController.java
@@ -0,0 +1,28 @@
+package views.gerente;
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import views.MainController;
+
+public class ChartsIngresosController {
+
+    private MainController mainController = new MainController();
+
+    @FXML
+    private Pane mainPanel;
+
+    @FXML
+    void toLogin(ActionEvent event) {
+        try {
+            mainController.toLogin(event);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
--- src/views/gerente/GerenteController.java
+++ src/views/gerente/GerenteController.java
@@ -1,9 +1,12 @@
 package views.gerente;
-
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import views.MainController;
 
 public class GerenteController {
@@ -14,6 +17,45 @@ public class GerenteController {
     void toLogin(ActionEvent event) {
         try {
             mainController.toLogin(event);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void ChartsIngresos(ActionEvent event){
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/views/gerente/ChartsIngresos.fxml"));
+            Scene scene = new Scene(root);
+            Pane panel = (Pane) scene.lookup("#mainPanel");
+            mainController.setUpScene(event, panel, scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void ChartsAsistencias(ActionEvent event){
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/views/gerente/ChartsAsistencia.fxml"));
+            Scene scene = new Scene(root);
+            Pane panel = (Pane) scene.lookup("#mainPanel");
+            mainController.setUpScene(event, panel, scene);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void ChartsLogistica(ActionEvent event){
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/views/gerente/ChartsLogistica.fxml"));
+            Scene scene = new Scene(root);
+            Pane panel = (Pane) scene.lookup("#mainPanel");
+            mainController.setUpScene(event, panel, scene);
         } catch (IOException e) {
             e.printStackTrace();
         }
